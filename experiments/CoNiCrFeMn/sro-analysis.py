017c88351b6efd70e1211d7b5838cc506ace3380
jwjeffr <jwjeffries2000@gmail.com>
2025-05-28 21:26:36 -0400

    sro run and analysis scripts

---
+++ experiments/CoNiCrFeMn/sro-analysis.py
@@ -0,0 +1,84 @@
+from itertools import combinations_with_replacement
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import numpy as np
+import ovito
+
+from cowley_sro_parameters import nearest_neighbor_topology, sro_modifier
+
+
+# type map constant info
+TYPE_MAP = {1: 'Co', 2: 'Ni', 3: 'Cr', 4: 'Fe', 5: 'Mn'}
+INVERSE_TYPE_MAP = {val: key for key, val in TYPE_MAP.items()}
+NUM_TYPES = len(TYPE_MAP)
+
+# cutoff info for SRO calculations
+NUM_NEAREST_NEIGHBORS = 12
+
+
+def main():
+
+    # create ovito pipeline, add a bonds modifier to create needed topology
+    pipeline = ovito.io.import_file("sim/cluster_expansion-*.dat")
+    nearest_neighbor_modifier = nearest_neighbor_topology(NUM_NEAREST_NEIGHBORS)
+    pipeline.modifiers.append(nearest_neighbor_modifier)
+
+    # create SRO modifier which calculates all SRO's
+    modifier = sro_modifier(type_map=TYPE_MAP)
+    pipeline.modifiers.append(modifier)
+
+    pairs = list(combinations_with_replacement(TYPE_MAP.values(), 2))
+    sro_params = np.zeros((pipeline.source.num_frames, NUM_TYPES, NUM_TYPES))
+    for frame in range(pipeline.source.num_frames):
+
+        data = pipeline.compute(frame)
+        for e1, e2 in pairs:
+            i, j = INVERSE_TYPE_MAP[e1], INVERSE_TYPE_MAP[e2]
+            sro_params[frame, i - 1, j - 1] = data.attributes[f'sro_{e1}{e2}']
+
+    fig = plt.figure()
+    gs = GridSpec(NUM_TYPES, NUM_TYPES, figure=fig, wspace=0.3, hspace=0.3)
+
+    for i in range(NUM_TYPES):
+        for j in range(i + 1):
+            t = TYPE_MAP[i + 1], TYPE_MAP[j + 1]
+            ax = plt.subplot(gs[i, j])
+            ax.set_ylim([-1.1, 1.1])
+            ax.set_xticks([0, 20, 40, 60, 80, 100])
+            ax.set_yticks([-1, -0.5, 0, 0.5, 1])
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.grid()
+            # along the left side of the grid, label y-axis ticks
+            if j == 0:
+                ax.set_yticklabels([-1, '', 0, '', 1])
+
+                # add y-axes label in the middle
+                if i == (NUM_TYPES + 1) // 2 - 1:
+                    ax.set_ylabel(r"$\alpha$-$\alpha'$ Cowley SRO parameter ($\chi_{\alpha\alpha'}$)")
+
+            # along the bottom side of the grid, label x-axis ticks
+            if i == NUM_TYPES - 1:
+                ax.set_xticklabels([0, '', '', '', '', 100])
+
+                # add x-axes label in the middle
+                if j == (NUM_TYPES + 1) // 2 - 1:
+                    ax.set_xlabel('frame')
+
+            # along the diagonal, label atom type names
+            if i == j:
+                ax.text(50, 1.3, r"$\alpha = $" + t[1], ha='center', va='bottom')
+                ax.text(110, 0.0, r"$\alpha' = $" + t[0], ha='left', va='center')
+            ax.plot(sro_params[:, j, i], color='black')
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    # need to use Agg or else matplotlib conflicts with ovito
+    mpl.use('TkAgg')
+    main()
---
+++ experiments/CoNiCrFeMn/sro-run.py
@@ -0,0 +1,91 @@
+import numpy as np
+from io import StringIO
+from pathlib import Path
+from string import Template
+
+from tce.structures import Supercell
+from tce.constants import LatticeStructure
+
+
+def main():
+
+    num_types = 5
+    supercell = Supercell(
+        lattice_parameter=3.59,
+        lattice_structure=LatticeStructure.FCC,
+        size=(10, 10, 10)
+    )
+    interaction_vector = np.loadtxt("CoNiCrFeMn-interaction-vector.txt")
+    generator = np.random.default_rng(seed=0)
+    types = generator.integers(num_types, size=supercell.num_sites)
+    state_matrix = np.zeros((supercell.num_sites, num_types), dtype=int)
+    state_matrix[np.arange(supercell.num_sites), types] = 1
+
+    beta = 38.68
+
+    num_steps = 1_000_000
+    dump_every = 10_000
+
+    template_path = Path("data-file-template.txt")
+    with template_path.open("r") as file:
+        template = Template(file.read())
+
+    for step in range(num_steps):
+
+        if not (step + 1) % dump_every:
+
+            _, types = np.where(state_matrix == 1)
+
+            with StringIO() as s:
+                position_data = np.vstack(
+                    (np.arange(1, supercell.num_sites + 1), types + 1, *supercell.positions.T,
+                     *np.zeros_like(supercell.positions.T))
+                ).T
+                np.savetxt(s, position_data, fmt=("%.0f", "%.0f", "%.3f", "%.3f", "%.3f", "%.0f", "%.0f", "%.0f"))
+                s.seek(0)
+                position_data = s.read()
+            with StringIO() as s:
+                velocity_data = np.vstack(
+                    (np.arange(1, supercell.num_sites + 1), *np.zeros_like(supercell.positions.T))
+                ).T
+                np.savetxt(s, velocity_data, fmt="%.0f")
+                s.seek(0)
+                velocity_data = s.read()
+
+            boxsize = supercell.lattice_parameter * np.array(supercell.size)
+            masses = [58.933, 58.69, 51.996, 55.847, 54.94]
+            data_str = template.substitute(
+                {
+                    "header": f"generated from {template_path}",
+                    "num_atoms": supercell.num_sites,
+                    "num_types": 5,
+                    "xlo": 0.0,
+                    "xhi": boxsize[0],
+                    "ylo": 0.0,
+                    "yhi": boxsize[1],
+                    "zlo": 0.0,
+                    "zhi": boxsize[2],
+                    "masses": "\n".join(f"{i + 1:.0f} {mass:.3f}" for i, mass in enumerate(masses)),
+                    "position_data": position_data,
+                    "velocity_data": velocity_data
+                }
+            )
+
+            with (Path("sim") / f"cluster_expansion-{step + 1:.0f}.dat").open("w") as file:
+                file.write(data_str)
+
+        new_state_matrix = state_matrix.copy()
+        i, j = generator.integers(supercell.num_sites, size=2)
+        new_state_matrix[i], new_state_matrix[j] = state_matrix[j], state_matrix[i]
+        feature_diff = supercell.clever_feature_diff(
+            state_matrix, new_state_matrix,
+            max_adjacency_order=2, max_triplet_order=1
+        )
+        energy_diff = np.dot(interaction_vector, feature_diff)
+        if np.exp(-beta * energy_diff) > 1.0 - generator.random():
+            state_matrix = new_state_matrix
+
+
+if __name__ == "__main__":
+
+    main()
