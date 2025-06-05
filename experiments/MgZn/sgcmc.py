be6c7b1dbff38cfe065e6237c2cdc87282c1779a
jwjeffr <jwjeffries2000@gmail.com>
2025-06-05 01:13:15 -0400

    full run data

--- experiments/MgZn/sgcmc.py
+++ experiments/MgZn/sgcmc.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 import numpy as np
 from ase.build import bulk
+from ase.io import lammpsdata
 import sparse
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -17,10 +20,10 @@ def main():
     beta = 1.0 / (8.617e-5 * temperature)
     interaction_vector = np.loadtxt("MgZn.txt")
 
-    num_steps = 10_000
+    num_steps = 100_000
     rng = np.random.default_rng(seed=0)
 
-    supercell = bulk('Mg', 'hcp', a=a, c=c).repeat((5, 5, 4))
+    supercell = bulk('Mg', 'hcp', a=a, c=c).repeat((15, 10, 10))
     distances = supercell.get_all_distances(mic=True)
 
     adjacency_tensors = sparse.stack([
@@ -36,6 +39,10 @@ def main():
 
     chemical_potentials_zn = np.linspace(-0.7, -0.1, 25)
     mg_concentration_first = np.zeros_like(chemical_potentials_zn)
+
+    dump_dir = Path("hysteresis")
+    dump_dir.mkdir(exist_ok=True)
+    type_map = ["Mg", "Zn"]
     for i, mu_zn in enumerate(chemical_potentials_zn):
 
         print(i)
@@ -67,6 +74,10 @@ def main():
                 state_matrix = new_state_matrix
 
         print()
+        _, types = np.where(state_matrix)
+        types = (type_map[t] for t in types)
+        supercell.set_chemical_symbols(types)
+        lammpsdata.write_lammps_data(dump_dir / f"first-mc-{i:.0f}.dat", supercell, masses=True, specorder=type_map)
         mg_concentration_first[i] = state_matrix[:, 0].mean()
 
     mg_concentration_second = np.zeros_like(chemical_potentials_zn)
@@ -81,6 +92,7 @@ def main():
         for step in range(num_steps):
 
             if not step % 1_000:
+
                 print(step)
 
             site = rng.integers(len(supercell))
@@ -101,6 +113,10 @@ def main():
                 state_matrix = new_state_matrix
 
         print()
+        _, types = np.where(state_matrix)
+        types = (type_map[t] for t in types)
+        supercell.set_chemical_symbols(types)
+        lammpsdata.write_lammps_data(dump_dir / f"second-mc-{i:.0f}.dat", supercell, masses=True, specorder=type_map)
         mg_concentration_second[i] = state_matrix[:, 0].mean()
 
     plt.plot(chemical_potentials_zn, 100 * mg_concentration_first)
