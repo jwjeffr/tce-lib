ec58291e61b0e3eeaa47836844791c244583e314
jwjeffr <jwjeffries2000@gmail.com>
2025-08-18 20:08:03 -0400

    added ase example

--- tce/__init__.py
+++ tce/__init__.py
@@ -3,14 +3,15 @@ r"""
 
 # Examples
 
-## 🐅 Placeholder
+## ⚛️ Using Atomic Simulation Environment (ASE)
 
-Below is a placeholder example
+Below is an example of converting an `ase.Atoms` object into a feature vector $\mathbf{t}$. The mapping is not exactly
+one-to-one, since an `ase.Atoms` object sits on a dynamic lattice rather than a static one, but we can regardless
+provide `tce-lib` sufficient information to compute $\mathbf{t}$.
 
 ```py
-.. include:: ../examples/simple.py
+.. include:: ../examples/using-ase.py
 ```
-
 """
 
 __version__ = "0.0.1"
